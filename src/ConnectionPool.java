1c65cdafc8b681753d1377f9541b8df79e1cc2cf
akeshava <akeshava@linkedin.biz>
2023-06-02 13:45:04 -0700

    Implement ConnectionPool singleton for managing database connections

---
+++ src/ConnectionPool.java
@@ -0,0 +1,49 @@
+import java.sql.Connection;
+import java.util.List;
+
+/**
+ * Singleton class representing a connection pool for managing database connections.
+ */
+public class ConnectionPool {
+  private static ConnectionPool instance;
+  private List<Connection> connections;
+
+  /**
+   * Private constructor to prevent direct instantiation of the class.
+   * Initializes the connection pool.
+   */
+  private ConnectionPool() {
+    // Initialize Connection
+  }
+
+  /**
+   * Returns the single instance of the ConnectionPool.
+   * If the instance does not exist, it creates a new one.
+   *
+   * @return The ConnectionPool instance
+   */
+  public static synchronized ConnectionPool getInstance() {
+    if(instance == null) {
+      instance = new ConnectionPool();
+    }
+    return instance;
+  }
+
+  /**
+   * Retrieves a connection from the pool.
+   *
+   * @return A database connection
+   */
+  public synchronized void getConnection() {
+    // Retrieve and return a connection from the pool
+  }
+
+  /**
+   * Releases a connection back to the pool.
+   *
+   * @param connection The database connection to release
+   */
+  public synchronized void releaseConnection(Connection connection) {
+    // Release the connection back to the pool
+  }
+}
