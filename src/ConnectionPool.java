03f63dba8293fcd70fca6ee23ab99e9d2b0c6320
akeshava <akeshava@linkedin.biz>
2023-06-14 12:44:05 -0700

    Add comments to explain Singelton for Connection Pool

--- src/ConnectionPool.java
+++ src/ConnectionPool.java
@@ -3,6 +3,9 @@ import java.util.List;
 
 /**
  * Singleton class representing a connection pool for managing database connections.
+ * In this example, the ConnectionPool class represents a resource manager for a connection pool.
+ * The getInstance() method returns the singleton instance of the ConnectionPool,
+ * ensuring that all parts of the application use the same pool of connections.
  */
 public class ConnectionPool {
   private static ConnectionPool instance;
